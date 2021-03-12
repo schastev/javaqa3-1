e70e02d974ab123ccf7ed87580019dbf773e79e1
k-emiko <62057364+k-emiko@users.noreply.github.com>
2021-03-12 23:17:24 +1100

    relocated logic to a separate class for hw 2.2.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int payment = Integer.parseInt(args[0]); //to use this with a CL argument instead of changing the variable and recompiling
-        int miles = 0;
-        miles = payment / 20;
+        int price = Integer.parseInt(args[0]); //to use this with a CL argument instead of changing the variable and recompiling
+        BonusMilesService service = new BonusMilesService();
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
